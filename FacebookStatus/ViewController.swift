710395dfdd9a2ca01810d6f356522755b63ea3a9
Gayan <Gayan@Gayans-MacBook-Pro.local>
2015-04-03 21:53:38 -0230

    Initial commit

---
+++ FacebookStatus/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  FacebookStatus
+//
+//  Created by Gayan on 2015-04-03.
+//  Copyright (c) 2015 Gayan. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func StatusUpdate(sender: AnyObject) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook){
+            var controller = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            controller.setInitialText("Swift Fb Post Test")
+            self.presentViewController(controller, animated: true, completion: nil)
+        } else{
+            println("No Facebook account found on device")
+        }
+    }
+
+}
+
